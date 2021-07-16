bda89e3212936955a676f7eee0bc14b0e5bc64fa
vkuzmenkova <valentinakuzmenkova@gmail.com>
2021-07-16 15:09:48 +0300

    create reverse int

---
+++ reverseInt.py
@@ -0,0 +1,28 @@
+def reverse(self, x: int) -> int:
+    tmp = []
+    pow_10 = 1
+    sum = 0
+    is_negative = False
+
+    if x < 0:
+        is_negative = True
+        x = abs(x)
+
+    while x // 10 > 0:  # 1
+        tmp.append(x % 10)  # 3 2
+        x = x // 10  # 12 1
+        pow_10 *= 10  # 100 1000
+
+    tmp.append(x)
+
+    for i in tmp:
+        sum += i * pow_10
+        pow_10 /= 10
+
+    if sum > 2 ** 31:
+        return 0
+
+    if is_negative:
+        return -1 * int(sum)
+    else:
+        return int(sum)
